1265c690016a4cc566ee98a4c9292ec20c60a600
Yoscroll <ianhenry.c808@gmail.com>
2017-01-27 09:21:15 -1000

    fix alignment

--- js/app.js
+++ js/app.js
@@ -2,9 +2,4 @@ function press(idName) {
   console.log("hi");
   var box = document.getElementById(idName);
   box.style.transform = 'rotateY(180deg)';
-  box.style.backgroundImage = "url('../assets/back.jpg')";
-  box.style.backgroundSize= "cover";
-}
-function k(){
-  console.log("ate");
 }
