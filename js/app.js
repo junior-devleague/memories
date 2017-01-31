cf246514ef08fd09f07e9a9357c4fe4ca727d310
Yoscroll <ianhenry.c808@gmail.com>
2017-01-30 22:54:57 -1000

    Added matching code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,39 @@
+var turned = [];
+var pic=[1,1,2,2,3,3,4,4];
+var ids = ["one","two","three","four","five","six","seven","eight"];
+var pair = [];
+var points = 0;
+for (var i = 0; i < 8; i++) {
+	var randomNumber =  Math.floor(Math.random() * (pic.length));
+	console.log(pic[randomNumber]);
+	var back=document.getElementsByClassName("back")[i];
+	back.style.backgroundImage = "url('./assets/"+pic[randomNumber]+".png')";
+	pair.push([ids[i],pic[randomNumber]]);
+	pic.splice(randomNumber, 1);
+}
 function press(idName) {
-  console.log("hi");
-  var box = document.getElementById(idName);
-  box.style.transform = 'rotateY(180deg)';
+	var box = document.getElementById(idName);
+	box.style.transform = 'rotateY(180deg)';
+	if((turned[0]==idName||turned[1]==idName)==false)
+	{
+		turned.push(idName);
+	}
+	if(turned.length == 2){
+		setTimeout(function(){
+			var first =document.getElementById(turned[0]);
+			var second =document.getElementById(turned[1]);
+			var x =ids.indexOf(turned[0]);
+			var y =ids.indexOf(turned[1]);
+			first.style.transform = 'rotateY(0deg)';
+			second.style.transform = 'rotateY(0deg)';
+			if(pair[x][1]==pair[y][1])
+			{
+				first.parentNode.removeChild(first);
+				second.parentNode.removeChild(second);
+				points += 1;
+				document.getElementById("points").innerHTML="Correct: "+points;
+			}
+			turned = [];
+		}, 400);
+	}
 }
